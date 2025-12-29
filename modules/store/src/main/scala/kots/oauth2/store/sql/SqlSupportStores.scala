91088c69f0d699181f501bddade91608d6407caa
andreoss <andreoss@sdf.org>
2025-12-28 21:04:48 -0300

    Fold every relational adapter onto one session base

--- modules/store/src/main/scala/kots/oauth2/store/sql/SqlSupportStores.scala
+++ modules/store/src/main/scala/kots/oauth2/store/sql/SqlSupportStores.scala
@@ -1,11 +1,9 @@
 package kots.oauth2.store.sql
 
 import java.sql.Connection
-import java.sql.PreparedStatement
 import java.sql.ResultSet
 import java.sql.Timestamp
 
-import cats.effect.kernel.Resource
 import cats.effect.kernel.Sync
 import cats.syntax.flatMap._
 
@@ -26,40 +24,6 @@ import kots.oauth2.store.ConsentStore
 import kots.oauth2.store.KeyStore
 import kots.oauth2.store.PushedRequest
 import kots.oauth2.store.PushedRequestStore
-
-private[sql] abstract class SqlSessions[F[_]: Sync](connect: F[Connection]) {
-
-  protected def session[A](use: Connection => A): F[A] =
-    Resource
-      .make(connect)(connection => Sync[F].blocking(connection.close()))
-      .use(connection => Sync[F].blocking(use(connection)))
-
-  protected def withTransaction[A](connection: Connection)(work: => A): A = {
-    connection.setAutoCommit(false)
-    scala.util.Try(work) match {
-      case scala.util.Success(outcome) =>
-        connection.commit()
-        outcome
-      case failure @ scala.util.Failure(_) =>
-        connection.rollback()
-        failure.get
-    }
-  }
-
-  protected def execute(connection: Connection, sql: String, values: String*): Int = {
-    val statement = connection.prepareStatement(sql)
-    try {
-      values.zipWithIndex.foreach { case (value, index) => statement.setString(index + 1, value) }
-      statement.executeUpdate()
-    } finally statement.close()
-  }
-
-  protected def optional(statement: PreparedStatement, index: Int, value: Option[String]): Unit =
-    value match {
-      case Some(present) => statement.setString(index, present)
-      case None          => statement.setNull(index, java.sql.Types.VARCHAR)
-    }
-}
 
 final class SqlKeyStore[F[_]: Sync] private (connect: F[Connection])
     extends SqlSessions[F](connect)
